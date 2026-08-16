586326d7c8b9d5b6ff2d47469805bd676b5f361a
Kunal Sharma <68844042+Roshians@users.noreply.github.com>
2026-08-16 23:49:31 +0530

    Add solution: Maximum Subarray

---
+++ Day-3/Classwork/LC53.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        sum = nums[0]
+        maxi = nums[0]
+
+        for i in range(1,len(nums)):
+            sum = max(nums[i], nums[i]+sum)
+            maxi = max(maxi, sum)
+        return maxi
