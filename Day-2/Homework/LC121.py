1ed6b629c2048373b7b515234acdc6dcb88e945b
Kunal Sharma <68844042+Roshians@users.noreply.github.com>
2026-08-16 23:49:00 +0530

    Add solution: Best Time to Buy and Sell Stock

--- Day-2/Homework/LC121.py
+++ Day-2/Homework/LC121.py
@@ -1,3 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        
+        buyingPrice = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+                
+            if prices[i] > buyingPrice:
+                profit = max(profit, (prices[i] - buyingPrice))
+            if buyingPrice > prices[i]:
+                buyingPrice = prices[i]
+        return profit
